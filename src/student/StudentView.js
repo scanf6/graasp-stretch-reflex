780d49b15bb577c41707faec9780dafabf29e0a8
azizmashkour <azizmashkour@gmail.com>
2019-01-30 11:13:52 +0100

    fix: use side menu component instead

--- src/student/StudentView.js
+++ src/student/StudentView.js
@@ -2,46 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withNamespaces } from 'react-i18next';
-import { Col, Row } from 'reactstrap';
 import { AppState } from '../config/AppState';
-import Description from '../components/Controls/Description';
-import Preview from '../components/Preview/Preview';
+import SideMenu from '../components/Main/SideMenu';
 import './StudentView.css';
 
 class StudentView extends Component {
   static propTypes = {
     t: PropTypes.func.isRequired,
-    themeColor: PropTypes.string.isRequired,
-    showTitle: PropTypes.bool.isRequired,
   };
 
   state = AppState;
 
   render() {
     const { obserViewActive } = this.state;
-    const { t, themeColor, showTitle } = this.props;
-    const defaultColor = themeColor || '#0f94f8';
+    const { t } = this.props;
     return (
       <div className="main-container">
-        { showTitle ? (
-          <Row>
-            <Col md={12}>
-              <h1 className="lab-title" style={{ backgroundColor: defaultColor }}>{t('Lab Title')}</h1>
-            </Col>
-          </Row>
-        ) : ''
-        }
-        <Row className="app-loader">
-          <Col md={8} className="preview-container">
-            <Preview t={t} />
-          </Col>
-          <Col md={4} className="controls-container">
-            <Description
-              t={t}
-              obserViewActive={obserViewActive}
-            />
-          </Col>
-        </Row>
+        <SideMenu
+          obserViewActive={obserViewActive}
+          t={t}
+        />
       </div>
     );
   }
