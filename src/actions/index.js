d37ceb3adacab5c4f6dc130800ad9b6c6b728252
azizmashkour <azizmashkour@gmail.com>
2019-02-07 12:21:10 +0100

    feat: define and import all action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,4 @@
-import { SET_THEME_COLOR, SET_LANGUAGE, SET_TITLE_STATE } from './types';
-
-export function themeColor({ newColor }) {
-  return {
-    type: SET_THEME_COLOR,
-    payload: newColor,
-  };
-}
-
-export function defaultLang({ newLang }) {
-  return {
-    type: SET_LANGUAGE,
-    payload: newLang,
-  };
-}
-
-export function titleState({ showTitle }) {
-  return {
-    type: SET_TITLE_STATE,
-    payload: showTitle,
-  };
-}
+export * from './layout';
+export * from './context';
+export * from './appInstance';
+export * from './users';
